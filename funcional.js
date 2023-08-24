69363652a8092b5989c43f096bc5d04adb688403
Guilherme Miranda <guilmiranda50@gmail.com>
2023-08-24 13:54:32 -0300

    Att Js

--- funcional.js
+++ funcional.js
@@ -2,3 +2,23 @@ function digitar(num){
     var numero = document.getElementById('Result').innerHTML;
     document.getElementById('Result').innerHTML = numero + num;
 }
+
+function limpar(){
+    document.getElementById('Result').innerHTML = ""
+}
+
+function cancelar(){
+    var numero = document.getElementById('Result').innerHTML; 
+    document.getElementById('Result').innerHTML = numero.slice(0, numero.length - 1);
+}
+
+function caractere(){
+    var carac = document.getElementById('Result').innerHTML.length
+    var limite = 8
+    if (carac == limite){
+        var numero = document.getElementById('Result').innerHTML;
+        document.getElementById('Result').innerHTML = numero.slice(0, numero.length - 1);
+    }
+}
+
+
